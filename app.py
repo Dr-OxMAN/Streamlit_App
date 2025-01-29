6fe36694569ab2f78b9592511c9e8fc0eba7b936
Dr-OxMAN <arthur.moya@wits.ac.za>
2025-01-29 14:32:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ import numpy as np
 
 #if uploaded_file is not None:
     #st.sidebar.image(uploaded_file, width=256, caption="Dr OxMAN")
-st.sidebar.image("https://github.com/Dr-OxMAN/Streamlit_App/blob/main/OXMAN.jpg",
+st.sidebar.image("OXMAN.jpg",
                  width=256, caption="Dr OxMAN")
 
 name = "Dr. OxMAN"
